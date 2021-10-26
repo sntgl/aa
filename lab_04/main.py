57c2ce39488b43a2c7eaf5179bc82876e6e7ed76
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-10-26 23:50:28 +0300

    refactor style

--- lab_04/main.py
+++ lab_04/main.py
@@ -24,10 +24,12 @@ class Matrix:
     def randomize(self):
         self.matrix = []
         self.mult = 1
+
         for i in range(self.size):
             self.matrix.append([])
             for j in range(self.size):
                 self.matrix[-1].append(random.randint(-1000, 1000))
+
         print(f'Generated matrix  {self.matrix}')
         return self
 
@@ -41,13 +43,16 @@ def calculateDet(m: Matrix):
             mul = m.matrix[0][i] * m.mult
             if i % 2 == 1:
                 mul *= -1
+
             size = m.size - 1
             matrix = []
+
             for j in range(1, m.size):
                 matrix.append([])
                 for k in range(m.size):
                     if k != i:
                         matrix[-1].append(m.matrix[j][k])
+
             s += calculateDet(Matrix(size, mul, matrix))
     return s
 
@@ -56,6 +61,7 @@ def calculateDetThreading(matrixes, returnList: list):
     s = 0
     for m in matrixes:
         s += calculateDet(m)
+
     returnList.append(s)
 
 
@@ -71,8 +77,9 @@ class Solver:
 
     def solve(self):
         activeThreads = []
-        matrixPerThread = self.m.size / self.threadCount
         allMatrixes = []
+        matrixPerThread = self.m.size / self.threadCount
+
         for i in range(self.m.size):
             mul = self.m.matrix[0][i] * self.m.mult
             if i % 2 == 1:
@@ -85,24 +92,31 @@ class Solver:
                     if k != i:
                         matrix[-1].append(self.m.matrix[j][k])
             allMatrixes.append(Matrix(size, mul, matrix))
+
         startIndex = 0
         threadTasksCount = []
+
         for i in range(self.threadCount):
             endIndex = round(matrixPerThread * (i + 1))
             if i == self.threadCount - 1:  # последний thread
                 threadMatrixes = allMatrixes[startIndex:]
             else:
                 threadMatrixes = allMatrixes[startIndex:endIndex]
+
             startIndex = endIndex
             activeThreads.append(
                 Process(target=calculateDetThreading, args=(threadMatrixes, self.returnList,)))
             threadTasksCount.append(len(threadMatrixes))
+
         for thread in activeThreads:
             thread.start()
+
         for thread in activeThreads:
             thread.join()
+
         result = sum(self.returnList)
         self.returnList = self.threadManager.list()
+
         return result
 
 
@@ -111,6 +125,7 @@ def inputMatrix():
         print("Input square matrix size (one int digit): ", end="")
         size1 = int(input())
         mat1 = np.zeros((size1, size1), dtype=float)
+
         print("Input matrix: ")
         for i in range(size1):
             line = [float(num) for num in input().split(" ")]
@@ -119,22 +134,26 @@ def inputMatrix():
             for j in range(size1):
                 mat1[i][j] = line[j]
     except ValueError:
-        raise Exception("Неверный ввод")
+        raise Exception("Wrong input!")
+
     return Matrix(matrix=mat1.tolist(), size=size1)
 
 
 def ui():
     print(f'Type anything for input size, press enter for input matrix: ')
     a = input()
+
     if a != '':
         print(f'Input matrix size: ', end='')
         size = int(input())
         a = Solver(size=size)
     else:
         a = Solver(matrix=inputMatrix())
+
     t = time.time()
     print('True det', calculateDet(a.m))
     print(f'No threads, time = {time.time() - t}')
+
     threadCount = 1
     while threadCount <= 32:
         a.threadCount = threadCount
@@ -151,23 +170,28 @@ def tests():
     maxMatrixSize = 9
     maxThreadsCount = 32
     repeatCount = 10
+
     for i in range(minMatrixSize, maxMatrixSize + 1):
         sizes.append(i)
+
     while threadCount <= maxThreadsCount:
         print('threads', threadCount)
         timeResults = []
         a = Solver()
         a.threadCount = threadCount
+
         for i in range(minMatrixSize, maxMatrixSize + 1):
             t = 0
             for j in range(repeatCount):
                 a.m = Matrix(size=i).randomize()
+
                 tt = time.time()
                 a.solve()
                 t += time.time() - tt
             timeResults.append((time.time() - t) * 1000 / repeatCount)
         plt.plot(sizes, timeResults, label=f'{threadCount} поток(ов)')
         threadCount *= 2
+
     plt.legend(loc='upper left')
     plt.xlabel('Размерность')
     plt.ylabel('Время работы (мс)')
